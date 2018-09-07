ca442286d00cbc99c9cef19fd2e9ece8a351e4c3
Guillaume Ferrari <guillaume.ferrari@gmail.com>
2018-09-07 10:22:51 +0200

    Création de modèle de page

---
+++ template-acf.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Template Name: ACF
+*/
+?>
+<h1>Template ACF</h1>
+<?php get_header(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php the_content(); ?>
+<?php endwhile; ?>
+<?php endif; ?>
+<?php get_footer(); ?>
